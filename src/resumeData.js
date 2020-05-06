46980d940540c6612a0e6679d0c51430fdc6c2f4
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2020-05-06 06:29:38 +0430

    v0.2.3

---
+++ src/resumeData.js
@@ -0,0 +1,79 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Abolfazl Omrani",
+    "role": "Frontend & Mobile Developer With ReactJs & React Native and a little bit Backend Developing using ExpressJs",
+    "socialLinks":[
+        {
+          "name":"telegram",
+          "url":"https://t.me/ab01f421/",
+          "className":"fab fa-telegram "
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/darka1pha",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://instagram.com/_abolfazl.omrani_",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently Studying Software Engineering at Shahid bahonar University of Kerman.",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"ReactNative"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"Expressjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"PES Competition",
+        "description":"React Front-End Website",
+        "imgurl":"images/portfolio/project1.png",
+        "projectUrl":"https://github.com/darka1pha/pes_competition"
+        
+      },
+      {
+        "name":"Contact keeper",
+        "description":"Full Stack App using React Express MongoDB ContextAPI",
+        "imgurl":"images/portfolio/project2.png",
+        "projectUrl":"https://github.com/darka1pha/contact-keeper"
+      },
+      {
+        "name":"IT Logger",
+        "description":"React App with Redux & JsonServer",
+        "imgurl":"images/portfolio/project3.png",
+        "projectUrl":"https://github.com/darka1pha/it_logger"
+      },
+      {
+        "name":"Github Finder",
+        "description":"React App for Searching Github Accounts",
+        "imgurl":"images/portfolio/project4.png",
+        "projectUrl":"https://github.com/darka1pha/github-finder"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Leave Your Web Development projects to me.",
+        "name":"React Js"
+      },
+      {
+        "description":"Leave Your Mobile App Development projects to me.",
+        "name":"React Native"
+      }
+    ]
+  }
+  
+  export default resumeData
