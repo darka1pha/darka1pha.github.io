3254f89bb064ed77ff58c30a5250f919abde5178
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2020-05-06 06:33:03 +0430

    v0.2.2

---
+++ build/precache-manifest.0dc2db45168befeda065f6f4f701422f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d4c8896b77ccb50c788",
+    "url": "/static/css/main.360099ea.chunk.css"
+  },
+  {
+    "revision": "8d4c8896b77ccb50c788",
+    "url": "/static/js/main.8d4c8896.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31fec7895151199232467cb21b3e8e54",
+    "url": "/index.html"
+  }
+];
